46800c9b17a6bf7a5f8ecf315123726ee35db3fa
ShubhamDev021 <shubhat021@gmail.com>
2025-05-22 19:16:50 +0530

    Implemented AAA methodology in xUnit

--- xUnitWebApplication.Tests/Controllers/HomeControllerTest.cs
+++ xUnitWebApplication.Tests/Controllers/HomeControllerTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using xUnitWebApplication.Controllers;
 
 namespace xUnitWebApplication.Tests.Controllers
 {
@@ -11,7 +12,16 @@ namespace xUnitWebApplication.Tests.Controllers
         [Fact]
         public void HomeController_Index_ValidResult()
         {
+            //AAA methodology
+            //Arrange
+            HomeController controller = new HomeController();
+            string expectedResult = "I am in Home Controller";
 
+            //Act
+            string actualResult = controller.Index();
+
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
         }
     }
 }
