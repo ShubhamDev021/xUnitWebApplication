ca33f00a9fa37ddb4cd3aa34cd475f2cc3702cbb
ShubhamDev021 <shubhat021@gmail.com>
2025-05-22 18:50:52 +0530

    Added Home Controller

---
+++ xUnitWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xUnitWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string Index()
+        {
+            return "I am in Home Controller";
+        }
+    }
+}
